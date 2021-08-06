b99c6de35c10623c53274b4afa3f6d3cc7c7e07e
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-05 23:26:19 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('web 44 rocks!')
+require ('dotenv').config() // this allows to inject fake invironment variables 
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require ('helmet')
+
+const server = express()
+server.use(express.json())// teaches express to parse req.body
+server.use(cors())
+server.use(helmet())
+
+server.use('*', (req, res) => {
+    res.json({
+        message: 'web44 is awsome indeed'
+    })
+})
+
+server.listen(5000, () => {
+    console.log(`listening on ${5000}`)
+})
